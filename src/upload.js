9a618c4d7c1208b9f50592372e1707db43c067af
Bakytbek Tatibekov <bakytbek.tatibekov@gmail.com>
2023-02-06 13:03:54 -0800

    FOODRATES: uploding umage

--- src/upload.js
+++ src/upload.js
@@ -14,25 +14,34 @@ module.exports.handler = async (event) => {
     }),
   };
 
-  // try {
-  //     const parsedBody = JSON.parse(event.body);
-  //     const base64File = parsedBody.file;
-  //     const decodedFile = Buffer.from(base64File.replace(/^data:image\/\w+;base64,/, ""), "base64");
-  //     const params = {
-  //         Bucket: BUCKET_NAME,
-  //         Key: `images/${new Date().toISOString()}.jpeg`,
-  //         Body: decodedFile,
-  //         ContentType: "image/jpeg",
-  //     };
+  try {
+    const parsedBody = JSON.parse(event.body);
+    const base64File = parsedBody.file;
+    const decodedFile = Buffer.from(
+      base64File.replace(/^data:image\/\w+;base64,/, ""),
+      "base64"
+    );
+    const params = {
+      Bucket: BUCKET_NAME,
+      Key: `images/${new Date().toISOString()}.jpeg`,
+      Body: decodedFile,
+      ContentType: "image/jpeg",
+    };
 
-  //     const uploadResult = await s3.upload(params).promise();
+    const uploadResult = await s3.upload(params).promise();
 
-  //     response.body = JSON.stringify({ message: "Successfully uploaded file to S3", uploadResult });
-  // } catch (e) {
-  //     console.error(e);
-  //     response.body = JSON.stringify({ message: "File failed to upload", errorMessage: e });
-  //     response.statusCode = 500;
-  // }
+    response.body = JSON.stringify({
+      message: "Successfully uploaded file to S3",
+      uploadResult,
+    });
+  } catch (e) {
+    console.error(e);
+    response.body = JSON.stringify({
+      message: "File failed to upload",
+      errorMessage: e,
+    });
+    response.statusCode = 500;
+  }
 
   return response;
 };
